791b9c9216f0082df80fc5a2b054a6dd14b4ea63
Matthias Weber <m.python@mailbox.org>
2019-11-07 23:36:36 +0100

    Update main.py

--- docker/data/app/main.py
+++ docker/data/app/main.py
@@ -1,15 +1,5 @@
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
-
 """
-Simple Bot to reply to Telegram messages.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+Based on basic Echobot example
 
 Add command descriptions
 https://stackoverflow.com/questions/34457568/how-to-show-options-in-telegram-bot
